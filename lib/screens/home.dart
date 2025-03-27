f7c538d8ea7bde467218f6ab08d3046edfd2cd6a
JoseGamino19 <115297933+JoseGamino19@users.noreply.github.com>
2025-03-27 15:06:48 -0700

    Add files via upload
    diseño mejorado

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:app/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:app/widgets/custom_drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,9 +14,24 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const CustomDrawer(), 
-      body: Center(child: Text("Pantalla Principal")),
+      backgroundColor: Color.fromRGBO(62, 75, 81, 1),
+      appBar: AppBar(backgroundColor: Color.fromRGBO(62, 75, 81, 1)),
+      drawer: const CustomDrawer(),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/home.png'),
+            Text(
+              'Bienvenido al nido ${authService.value.currentUser!.displayName}',
+              style: GoogleFonts.dancingScript(
+                fontWeight: FontWeight.bold,
+                fontSize: 40, // Tamaño más grande para destacar
+                color: Colors.white, // Letras blancas
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
