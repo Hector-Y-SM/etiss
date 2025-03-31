4fde092f777da5f0bbd0ec67fa993316850aecf3
JoseGamino19 <115297933+JoseGamino19@users.noreply.github.com>
2025-03-31 11:06:55 -0700

    Add files via upload

--- lib/widgets/input_file.dart
+++ lib/widgets/input_file.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
 Widget inputFile({
-    required String label,
-    required TextEditingController controller,
-    bool obscureText = false,
-    String? hintText,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
-        controller: controller,
-        obscureText: obscureText,
-        decoration: InputDecoration(
-          hintText: hintText,
-          labelText: label,
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        ),
-        validator: (value) => value == null || value.isEmpty ? 'Por favor ingresa $label' : null,
+  required String label,
+  required TextEditingController controller,
+  bool obscureText = false,
+  String? hintText,
+  String? Function(String?)? validator,
+}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: label,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
       ),
-    );
-  }
+      validator: validator,
+    ),
+  );
+}
