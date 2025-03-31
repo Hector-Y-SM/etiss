39f82d8c9b5d906adce7b4df26f3f2197b649df1
Hector Yahir <121281373+Hector-Y-SM@users.noreply.github.com>
2025-03-31 13:37:08 -0700

    Add files via upload

---
+++ change_email_screen.dart
@@ -0,0 +1,153 @@
+import 'package:app/models/auth_service.dart';
+import 'package:app/screens/profile_data.dart';
+import 'package:app/widgets/input_file.dart';
+import 'package:flutter/material.dart';
+
+class ChangeEmailScreen extends StatefulWidget {
+  const ChangeEmailScreen({super.key});
+
+  @override
+  State<ChangeEmailScreen> createState() => _ChangeEmailScreenState();
+}
+
+class _ChangeEmailScreenState extends State<ChangeEmailScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _newEmail = TextEditingController();
+  final _password = TextEditingController();
+
+  Future<void> _updateEmail() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await authService.value.updateEmailUser(
+          oldEmail: authService.value.currentUser!.email as String,
+          newEmail: _newEmail.text,
+          password: _password.text,
+        );
+
+        _showSnackBar(
+          'Correo actualizado correctamente',
+          const Color.fromARGB(255, 26, 184, 31),
+        );
+
+        Navigator.pop(context);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ProfileData()),
+        );
+      } catch (e) {
+        _showSnackBar(_getError(e.toString()), Colors.red);
+      }
+    }
+  }
+
+  void _showSnackBar(String message, Color color) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message), backgroundColor: color),
+    );
+  }
+
+  String _getError(String errorCode) {
+    switch (errorCode) {
+      case 'Exception: error [firebase_auth/invalid-new-email] Error':
+        return 'El correo electrónico no es válido.';
+      case 'Exception: error [firebase_auth/too-many-requests]':
+        return 'Demasiados intentos. Intenta más tarde.';
+      case 'Exception: error [firebase_auth/invalid-credential]':
+        return 'Contraseña no válida. Se requiere al menos 6 caracteres.';
+      default:
+        return 'Error. Intenta más tarde nuevamente.';
+    }
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Ingrese un correo válido';
+    }
+    final emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegex.hasMatch(value)) {
+      return 'El formato del correo no es válido';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == '') {
+      return 'por favor ingresa tu contraseña';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 40,
+                height: 5,
+                margin: const EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              const Text(
+                "Cambiar correo",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                readOnly: true,
+                controller: TextEditingController(
+                  text: authService.value.currentUser!.email,
+                ),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              InputFile(
+                label: "Nuevo correo",
+                controller: _newEmail,
+                validator: _validateEmail,
+              ),
+              InputFile(
+                label: "Contraseña",
+                controller: _password,
+                obscureText: true,
+                validator: _validatePassword,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _updateEmail,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  "Actualizar correo",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ login_form.dart
@@ -0,0 +1,225 @@
+import 'package:app/models/auth_service.dart';
+import 'package:app/screens/forgot_password.dart';
+import 'package:app/screens/home.dart';
+import 'package:app/widgets/input_file.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  String? _errorMessage;
+
+  void _login() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _errorMessage = null;
+      });
+
+      try {
+        await authService.value.signIn(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+
+        setState(() {
+          _errorMessage = 'Inicio de sesión exitoso';
+        });
+
+        Future.delayed(const Duration(seconds: 1), () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Home()),
+          );
+        });
+      } on FirebaseAuthException catch (e) {
+        print('error ${e.code}');
+        setState(() {
+          _errorMessage = _getError(e.code);
+        });
+      } catch (errorCode) {
+        setState(() {
+          _errorMessage = 'Error inesperado.Intente mas tarde por favor';
+        });
+      }
+    }
+  }
+
+  String _getError(String errorCode) {
+    switch (errorCode) {
+      case 'invalid-email':
+        return 'El correo electrónico no es válido.';
+      case 'user-not-found':
+        return 'No se encontró una cuenta con este correo.';
+      case 'too-many-requests':
+        return 'Demasiados intentos. Intenta más tarde.';
+      case 'invalid-credential':
+        return 'Contraseña incorrecta';
+      default:
+        return 'Error al iniciar sesión. Intenta nuevamente.';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 8, // Sombra del card
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20), // Bordes redondeados
+        ),
+        color: const Color.fromARGB(204, 255, 255, 255), // Fondo blanco con transparencia
+        child: Padding(
+          padding: const EdgeInsets.all(20.0), // Espaciado interno del card
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Título "Iniciar sesión"
+                Text(
+                  "Iniciar sesión",
+                  style: GoogleFonts.roboto(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black, // Letras negras
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Campo de correo electrónico
+                InputFile(
+                  label: "Correo electrónico",
+                  controller: _emailController,
+                ),
+                const SizedBox(height: 16),
+                // Campo de contraseña
+                InputFile(
+                  label: "Contraseña",
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16),
+                // Centrar el enlace de "¿Olvidaste tu contraseña?"
+                Align(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ForgotPassword(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "¿Olvidaste tu contraseña?",
+                      style: TextStyle(
+                        color: Colors.black, // Letras negras
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                // Botón de Iniciar Sesión
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: _login,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.black), // Borde negro
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Text(
+                    "Iniciar Sesión",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.black, // Letras negras
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Mensaje de _errorMessage o éxito
+                if (_errorMessage != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      _errorMessage!,
+                      style: TextStyle(
+                        color:
+                            _errorMessage == 'Inicio de sesión exitoso'
+                                ? Colors.green
+                                : Colors.red,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget para los campos de entrada
+  Widget inputFile({
+    required String label,
+    required TextEditingController controller,
+    bool obscureText = false,
+    TextInputType keyboardType = TextInputType.text,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            label,
+            style: GoogleFonts.roboto(
+              color: Colors.black, // Letras negras
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(height: 5),
+          TextFormField(
+            controller: controller,
+            obscureText: obscureText,
+            keyboardType: keyboardType,
+            style: const TextStyle(
+              color: Colors.black, // Texto dentro del campo en negro
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 0,
+                horizontal: 10,
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black), // Borde negro
+              ),
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black), // Borde negro
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Por favor ingresa $label';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
